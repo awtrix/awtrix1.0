7d8125c73067ad1ff9c42d3bb1fbfdcab8b80af0
Christopher Mühl <christopher@dieschittigs.de>
2018-02-09 09:02:08 +0100

    Make indentation and code style consistent

--- src/wifi.ino
+++ src/wifi.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <EEPROM.h>
-#include <DNSServer.h>            //Local DNS Server used for redirecting all rs to the configuration portal
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <DNSServer.h>            // Local DNS Server used for redirecting all rs to the configuration portal
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ESP8266mDNS.h> 
 
 #define MAX_SRV_CLIENTS 3
@@ -13,10 +13,9 @@ String IP_ADRESS;
 int localUdpPort = 52829;  // local port to listen on
 char inputBuffer[512];     // buffer for incoming packets
 
-
 void wifiSetup() {
     Serial.println("Setup WiFi");
-    matrixText(true,"WiFi",5,0,0,0,255);
+    matrixText(true, "WiFi", 5, 0, 0, 0, 255);
     WiFiManager wifiManager;
     wifiManager.setTimeout(120);
     wifiManager.autoConnect("AWTRIX");
@@ -26,14 +25,16 @@ void wifiSetup() {
     Serial.println(IP_ADRESS);
     Udp.begin(localUdpPort);
     if (!MDNS.begin("awtrix")) { 
-      Serial.println("Error setting up MDNS responder!");
+        Serial.println("Error setting up MDNS responder!");
     }
     Serial.println("mDNS responder started");
-    if (SHOW_IP_ON_BOOT) {matrixBoot(IP_ADRESS);};
+    if (SHOW_IP_ON_BOOT) {
+        matrixBoot(IP_ADRESS);
+    }
+        
     MDNS.addService("http", "tcp", 80);
- }
+}
 
- 
 void wifiReset() {
     Serial.println("Reset");
     WiFi.disconnect();
@@ -45,32 +46,29 @@ void wifiReset() {
 void udpLoop() {
     int packetSize = Udp.parsePacket();
     if (packetSize) {
-     
-      Udp.read(inputBuffer, 256);
-      Serial.println("Contents:");
-      Serial.println(inputBuffer);
-      
-        if (packetSize > 0)
-      {
-        inputBuffer[packetSize] = 0;
-      }
-      doJob(inputBuffer);
+        Udp.read(inputBuffer, 256);
+        Serial.println("Contents:");
+        Serial.println(inputBuffer);
+
+        if (packetSize > 0) {
+            inputBuffer[packetSize] = 0;
+        }
+
+        doJob(inputBuffer);
     }
+
     delay(10);
 }
 
-void tcpLoop(){
-  server.handleClient();
-  }
-
-
-void sendMSG(String msg){
-
-  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  Udp.print(msg);
-  Udp.endPacket();
+void tcpLoop() {
+    server.handleClient();
 }
 
+void sendMSG(String msg) {
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.print(msg);
+    Udp.endPacket();
+}
 
 //LOOP FOR LIVE DRAWING VIA UDP
 //void udpLoop() {
@@ -96,8 +94,3 @@ void sendMSG(String msg){
 //    Udp.endPacket();
 //  }
 //}
-
-
-
-
-
