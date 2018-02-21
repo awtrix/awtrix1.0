0fc3f7d5ad955cccf1945f85991400234a43857b
Blueforcer <muehls@int.dinotec.de>
2018-02-21 15:44:36 +0100

    Adding temp icon

--- lib/Applications/DHT22App.cpp
+++ lib/Applications/DHT22App.cpp
@@ -4,16 +4,19 @@ const unsigned long UpdateThreshold = 100000l;
 float humidity;
 float temperature;
 
-static unsigned char rain[]={0x80,0x10,0x40,0x0,0x90,0x0,0x28,0x80};
-static unsigned char snow[]={0x63,0x55,0x36,0x8,0x36,0x55,0x63,0x0};
+static unsigned char temp[]={0x1c22221414141408};
+static unsigned char hum[]={0x63,0x55,0x36,0x8,0x36,0x55,0x63,0x0};
 
 
 void DHT22App::render(DisplayManager& display) {
+    display.drawBitmap(temp, {0, 0}, {255, 0, 0}, 8, 8);
     display.drawText(String(temperature) + "\xA7", {12, 0}, {0xeb, 0xb7, 0x3d}, false);
 }
 
 void DHT22App::enable() {
-   dht.setup(SENSOR_PIN);
+    dht.setup(SENSOR_PIN);
+    humidity= dht.getHumidity();
+    temperature= dht.getTemperature();
 }
 
 void DHT22App::tick(unsigned long delta) {
