a8799518d9e5b0fb3dc1d58ebdb6fbded0ac3a27
Blueforcer <muehls@int.dinotec.de>
2018-02-26 16:18:04 +0100

    implement drawApp function

--- lib/Awtrix/DisplayManager.h
+++ lib/Awtrix/DisplayManager.h
@@ -33,7 +33,7 @@ class DisplayManager
         }
 
         bool executeCommand(command_t, String);
-
+        void drawApp(const uint16_t[], String,AwtrixPosition, AwtrixColor, bool, bool, int, int);
         void clear();
         void refresh();
         void rotate(int);
