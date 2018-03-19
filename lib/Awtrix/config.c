c542d93defcb73c69ed39fe19672ef9834701df0
Blueforcer <muehls@int.dinotec.de>
2018-03-19 15:25:37 +0100

    add config.c

---
+++ lib/Awtrix/config.c
@@ -0,0 +1,29 @@
+    int SHOW_IP_ON_BOOT                 = 1;
+    int AUTO_BRIGHTNESS                 = 0;
+    int BRIGHTNESS                      = 100;
+    int UTC_OFFSET                      = 1;
+    int BIG_TIME                        = 1;
+    int PET_MOOD                        = 1;
+    int AUTO_UPDATE                     = 0;
+    char TEXT_COLOR_R                   = 255;
+    char TEXT_COLOR_G                   = 255;
+    char TEXT_COLOR_B                   = 255;
+    int SCROLL_SPEED                    = 30;
+
+    const char* WUNDERGROUND_API_KEY    = "";
+    const char* WUNDERGROUND_LANGUAGE   = "DE";
+    const char* WUNDERGROUND_ZMW_CODE   = "";
+
+    const char* YT_API_KEY              = "";
+    const char* YT_CHANNEL_ID           = "";
+    
+    const char* FB_API_URL              = "";
+    const char* FINGERPRINT             = "";
+
+    const char* MQTT_SERVER             = "";
+    int MQTT_PORT                       = 1883;
+    const char* MQTT_USERNAME           = "";
+    const char* MQTT_PASSWORD           = "";
+
+    const char* BLYNK_KEY               = "";
+    
