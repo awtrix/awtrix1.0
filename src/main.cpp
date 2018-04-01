37a2ee9028cd872ad3d79111dc799d57b3d25b42
Blueforcer <emule@gmx.net>
2018-04-01 18:45:57 +0200

    WifiCheck

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,22 @@ void setup() {
      
 }
 
+const char* wl_status_to_string(wl_status_t status) {
+  switch (status) {
+    case WL_NO_SHIELD: return "WL_NO_SHIELD";
+    case WL_IDLE_STATUS: return "WL_IDLE_STATUS";
+    case WL_NO_SSID_AVAIL: return "WL_NO_SSID_AVAIL";
+    case WL_SCAN_COMPLETED: return "WL_SCAN_COMPLETED";
+    case WL_CONNECTED: return "WL_CONNECTED";
+    case WL_CONNECT_FAILED: return "WL_CONNECT_FAILED";
+    case WL_CONNECTION_LOST: return "WL_CONNECTION_LOST";
+    case WL_DISCONNECTED: return "WL_DISCONNECTED";
+  }
+}
+
+  
+
+
 
 
 
@@ -71,6 +87,8 @@ void loop() {
             if (SLEEP_MODE_ACTIVE) NTPclient.checkSleepMode();
             }
         }
+       
     }
+
 
 
