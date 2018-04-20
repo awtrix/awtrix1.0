7d80d3e7f7a1da5e430231a52f2ee5f7eecc215f
Blueforcer <emule@gmx.net>
2018-04-21 01:08:03 +0200

    send ACK

--- lib/Awtrix/AwtrixUDP.cpp
+++ lib/Awtrix/AwtrixUDP.cpp
@@ -17,7 +17,7 @@ void AwtrixUDP::loop() {
     int packetSize = Udp.parsePacket();
     if (packetSize) {
       Udp.read(inputBuffer, 512);
-      sendMSG("ACK");
+
       if (packetSize > 0)
         {
           inputBuffer[packetSize] = 0;
@@ -27,79 +27,91 @@ void AwtrixUDP::loop() {
         String command = dat.substring(0,dat.indexOf("%"));
         String payload = dat.substring(dat.indexOf("%")+1,dat.length());
 
-      if (command == "bri"){   
-        BRIGHTNESS=payload.toInt();
-        DisplayManager::getInstance().setBrightness(payload.toInt());
-        return; 
+      if (command == "bri"){ 
+          sendMSG("ACK");  
+          BRIGHTNESS=payload.toInt();
+          DisplayManager::getInstance().setBrightness(payload.toInt());
+          return; 
       }
 
       if (command== "save"){
+          sendMSG("ACK");
           AwtrixSettings::getInstance().saveSettings();
           return;
       }
 
       if (command== "reset"){
+          sendMSG("ACK");
           ESP.reset();
           return;
       }
 
       if (command== "mood"){
+          sendMSG("ACK");
           PET_MOOD=payload.toInt();
           return;
       }
 
       if (command== "pong"){
-         PONG_ACTIVE=payload.toInt();
-         return;
+          sendMSG("ACK");
+          PONG_ACTIVE=payload.toInt();
+          return;
       }
 
       if (command== "snake"){
-         SNAKE_ACTIVE=payload.toInt();
-         return;
+          sendMSG("ACK");
+          SNAKE_ACTIVE=payload.toInt();
+          return;
       }
 
       if (command== "pongmove"){
-         paddle=payload.toInt();
-         return;
+          sendMSG("ACK");
+          paddle=payload.toInt();
+          return;
       }
 
       if (command== "snakemove"){
-         direction=payload.toInt();
-         return;
+          sendMSG("ACK");
+          direction=payload.toInt();
+          return;
       }
 
       if (command== "text"){
+          sendMSG("ACK");
           DisplayManager::getInstance().scrollText(payload);
           return;
       }
 
       if (command== "next"){
-         //ApplicationManager::getInstance().nextApplication() ;
-         return;
+          sendMSG("ACK");
+          //ApplicationManager::getInstance().nextApplication() ;
+          return;
       }
 
       if (command== "game"){
-         game=payload.toInt();
-         return;
+          sendMSG("ACK");
+          game=payload.toInt();
+          return;
       }
 
-            if (command== "gamemode"){
-         gamemode=payload.toInt();
-         return;
+      if (command== "gamemode"){
+          sendMSG("ACK");
+          gamemode=payload.toInt();
+          return;
       }
 
-     
       if (command == "color"){
-        long number = (long) strtol( &payload[1], NULL, 16);
-        TEXT_COLOR_R = number >> 16;
-        TEXT_COLOR_G = number >> 8 & 0xff;
-        TEXT_COLOR_B = number & 0xff;
-        DisplayManager::getInstance().setColor({TEXT_COLOR_R,TEXT_COLOR_G,TEXT_COLOR_B});
-        return;
+          sendMSG("ACK");
+          long number = (long) strtol( &payload[1], NULL, 16);
+          TEXT_COLOR_R = number >> 16;
+          TEXT_COLOR_G = number >> 8 & 0xff;
+          TEXT_COLOR_B = number & 0xff;
+          DisplayManager::getInstance().setColor({TEXT_COLOR_R,TEXT_COLOR_G,TEXT_COLOR_B});
+          return;
       }
-
 
       if (command == "settings"){
+          sendMSG("ACK");
         if (payload=="get"){
            sendMSG(AwtrixSettings::getInstance().loadSettings()); 
            
@@ -111,7 +123,6 @@ void AwtrixUDP::loop() {
         }
         return;
       }
-  sendMSG("ACK");
     }
     delay(10);
 }
